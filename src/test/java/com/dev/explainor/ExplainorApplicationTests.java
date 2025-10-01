efe1d4713cdd9042586c06c3dfff9b5fce4c524e
anton <jaggervid1@gmail.com>
2025-10-01 17:35:01 +0300

    Initial.

---
+++ src/test/java/com/dev/explainor/ExplainorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.explainor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplainorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
